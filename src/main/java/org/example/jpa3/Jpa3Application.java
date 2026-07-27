d53dc359405ec369943be81823707397db98ee5e
a1l1ke <onetenthplease@gmail.com>
2026-07-27 14:05:03 +0900

    JPA 감사와 QueryDSL 설정 분리

--- src/main/java/org/example/jpa3/Jpa3Application.java
+++ src/main/java/org/example/jpa3/Jpa3Application.java
@@ -2,10 +2,9 @@ package org.example.jpa3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class Jpa3Application {
 
     public static void main(String[] args) {
