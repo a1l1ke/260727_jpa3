399f4c974321b5ad18f295454b45f875bca08f0c
a1l1ke <onetenthplease@gmail.com>
2026-07-27 10:11:20 +0900

    휴대폰 엔티티와 저장소 계층 추가

---
+++ src/main/java/org/example/jpa3/repository/PhoneRepository.java
@@ -0,0 +1,11 @@
+package org.example.jpa3.repository;
+
+import org.example.jpa3.entity.Phone;
+
+import java.util.List;
+
+public interface PhoneRepository {
+    List<Phone> findAll();
+
+    void save(Phone phone);
+}
